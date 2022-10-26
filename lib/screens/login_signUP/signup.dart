9aa47a77919d55a4ba6f4509a3cadadd5ce93921
Yazeed Gazalat <105671860+YazeedGhazalat@users.noreply.github.com>
2022-10-26 16:55:16 +0300

    this is last update

--- lib/screens/login_signUP/signup.dart
+++ lib/screens/login_signUP/signup.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -9,25 +13,174 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  File? _image;
+  Future getImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+    final imageTemporary = File(image.path);
+    setState(() {
+      this._image = imageTemporary;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        titleSpacing: 10,
-        title: Row(
-          children: [
-            FaIcon(
-              size: 30,
-              FontAwesomeIcons.accusoft,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          titleSpacing: 10,
+          title: Row(
+            children: [
+              FaIcon(
+                size: 30,
+                FontAwesomeIcons.accusoft,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                "Stack",
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+        body: Form(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text("Welcome !",
+                        style: TextStyle(fontSize: 35),
+                        textAlign: TextAlign.start),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: Text(
+                        "Sign up to continue",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: phoneNumberController,
+                        decoration: const InputDecoration(
+                          labelText: 'Phone Number (optional)',
+                          labelStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 45, left: 45),
+                      child: ElevatedButton.icon(
+                        icon: const Icon(
+                            CupertinoIcons.photo_fill_on_rectangle_fill),
+                        onPressed: () {
+                          getImage();
+                        },
+                        label: const Text(
+                          "Upload your Photo",
+                        ),
+                      ),
+                    ),
+                    _image != null
+                        ? Image.file(
+                            _image!,
+                            width: 250,
+                            height: 250,
+                            fit: BoxFit.cover,
+                          )
+                        : Image.asset("images/person.png"),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 45, left: 45),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(200, 50),
+                          maximumSize: const Size(350, 50),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Sign Up Now",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        const Text(
+                          'Don\'t have an account?',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        TextButton(
+                          child: const Text(
+                            'Sign up',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          onPressed: () {},
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    )
+                  ]),
             ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              "Stack",
-              style: TextStyle(color: Colors.black),
-            ),
-          ],
+          ),
         ),
       ),
     );
