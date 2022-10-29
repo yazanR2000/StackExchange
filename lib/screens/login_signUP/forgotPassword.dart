4c3282dd0eeecefa388168a05b2ca1e4d9e7817b
Yazeed Gazalat <105671860+YazeedGhazalat@users.noreply.github.com>
2022-10-29 14:13:11 +0300

    login_sign_up folder edited

--- lib/screens/login_signUP/forgotPassword.dart
+++ lib/screens/login_signUP/forgotPassword.dart
@@ -17,114 +17,122 @@ class _forgotPasswordState extends State<forgotPassword> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          titleSpacing: 10,
-          title: Row(
-            children: [
-              FaIcon(
-                size: 30,
-                FontAwesomeIcons.accusoft,
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Scaffold(
+            appBar: AppBar(
+              titleSpacing: 10,
+              title: Row(
+                children: [
+                  FaIcon(
+                    size: constraints.maxHeight * 0.035,
+                    FontAwesomeIcons.accusoft,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Stack",
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ],
               ),
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                "Stack",
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-            ],
-          ),
-        ),
-        body: Form(
-          key: myFormKey,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text("Forgot Password!",
-                        style: TextStyle(fontSize: 35),
-                        textAlign: TextAlign.start),
-                    SizedBox(
-                      height: 150,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        textInputAction: TextInputAction.next,
-                        validator: ((value) {
-                          // Check if this field is empty
-                          if (value == null || value.isEmpty) {
-                            return 'This field is required';
-                          }
+            ),
+            body: Form(
+              key: myFormKey,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text("Forgot Password!",
+                            style: TextStyle(
+                                fontSize: constraints.maxHeight * .045),
+                            textAlign: TextAlign.start),
+                        SizedBox(
+                          height: 150,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            textInputAction: TextInputAction.next,
+                            validator: ((value) {
+                              // Check if this field is empty
+                              if (value == null || value.isEmpty) {
+                                return 'This field is required';
+                              }
 
-                          if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
-                            return "Please enter a valid email address";
-                          }
+                              if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                                return "Please enter a valid email address";
+                              }
 
-                          // the email is valid
-                          return null;
-                        }),
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          labelStyle: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 20,
+                              // the email is valid
+                              return null;
+                            }),
+                            controller: emailController,
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                              labelStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 20,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 45, left: 45),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(200, 50),
-                          maximumSize: const Size(350, 50),
+                        SizedBox(
+                          height: 50,
                         ),
-                        onPressed: () async {
-                          if (myFormKey.currentState!.validate()) {
-                            try {
-                              await FirebaseAuth.instance
-                                  .sendPasswordResetEmail(
-                                      email: emailController!.text.trim());
-                              showDialog(
-                                  context: context,
-                                  builder: ((context) {
-                                    return AlertDialog(
-                                      content: Text("password reset link sent"),
-                                    );
-                                  }));
-                            } on FirebaseAuthException catch (e) {
-                              print(e);
-                              showDialog(
-                                  context: context,
-                                  builder: ((context) {
-                                    return AlertDialog(
-                                      content: Text(e.message.toString()),
-                                    );
-                                  }));
-                            }
-                          }
-                        },
-                        child: Text(
-                          "Send Email",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 45, left: 45),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(constraints.maxHeight * 0.2,
+                                  constraints.maxHeight * .06),
+                              maximumSize: Size(constraints.maxHeight * 0.25,
+                                  constraints.maxHeight * .07),
+                            ),
+                            onPressed: () async {
+                              if (myFormKey.currentState!.validate()) {
+                                try {
+                                  await FirebaseAuth.instance
+                                      .sendPasswordResetEmail(
+                                          email: emailController!.text.trim());
+                                  showDialog(
+                                      context: context,
+                                      builder: ((context) {
+                                        return AlertDialog(
+                                          content:
+                                              Text("password reset link sent"),
+                                        );
+                                      }));
+                                } on FirebaseAuthException catch (e) {
+                                  print(e);
+                                  showDialog(
+                                      context: context,
+                                      builder: ((context) {
+                                        return AlertDialog(
+                                          content: Text(e.message.toString()),
+                                        );
+                                      }));
+                                }
+                              }
+                            },
+                            child: Text(
+                              "Send Email",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  ]),
+                      ]),
+                ),
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
